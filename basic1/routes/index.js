6a3685d8e614fe8b6143151d6b4923beec856afb
Harshsharma008 <harshsharma01012002@gmail.com>
2024-02-03 17:07:24 +0530

    connection

--- basic1/routes/index.js
+++ basic1/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
+const userModel = require('./users');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+router.get('/create', async function(req, res, next) {
+  const createduser= await userModel.create({
+    username:"harsh",
+    age:24,
+    name:"harsh"
+  });
+  res.send(createduser);
 });
 
 module.exports = router;
