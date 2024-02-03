4cd45ca57053faeb5e1ff0697fc0af7be4640953
Harshsharma008 <harshsharma01012002@gmail.com>
2024-02-03 19:14:37 +0530

    session check

--- basic1/routes/index.js
+++ basic1/routes/index.js
@@ -6,14 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/create', async function(req, res, next) {
-  const createduser= await userModel.create({
-    username:"harsh",
-    age:24,
-    name:"harsh"
-  });
-  res.send(createduser);
-});
+// create the collection
+// router.get('/create', async function(req, res, next) {
+//   const createduser= await userModel.create({
+//     username:"harsh",
+//     age:24,
+//     name:"harsh"
+//   });
+//   res.send(createduser);
+// });
 
 // get all the data from the collections
 // router.get('/find', async function(req, res, next) {
@@ -25,5 +26,14 @@ router.get('/create', async function(req, res, next) {
 //   let user = await userModel.findOne({username:"harsh"});
 //   res.send(user);
 // })
+ router.get("/", function(req,res){
+  req.session.ban = true;
+  req.render("index");
+ });
+
+ router.get("/checkban", function(req,res){
+  console.log(req.session);
+  res.send("check kiya hai console dekho"); 
+ })
 
 module.exports = router;
