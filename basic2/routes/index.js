87d8c4e5c96c105e41aec330f4bf6160245f8319
Harshsharma008 <harshsharma01012002@gmail.com>
2024-02-04 12:25:16 +0530

    connection created and create operation

--- basic2/routes/index.js
+++ basic2/routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var userModel= require('./users');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
 });
 
+// flash m data bnaya
+router.get('/failed', function(req, res) {
+  req.flash("age",21);
+  res.send("bn gya ");
+});
+ 
+
+// check kiya ki data aata  h kya flash se
+router.get('/check', function(req, res) {
+  console.log(req.flash("age"));
+  res.send("backend k console p");
+});
+
+
+router.get('/create',async function(req,res){
+  let userdata = await userModel.create({
+    username:"harsh",
+    nickname: "String",
+    description: "i am a developer",
+    categories:['js','node'],
+    
+  });
+  res.send(userdata);
+})
 module.exports = router;
